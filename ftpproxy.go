74f98131ee8a0a052aa3d7fffa16ddab64111942
junjie.li <junjie.li@beebank.com>
2017-02-05 01:31:32 +0800

    1. 添加ftp支持，未完成

---
+++ ftpproxy.go
@@ -0,0 +1,56 @@
+package sproxy
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"github.com/smallfish/ftp"
+	"net/url"
+	"strconv"
+)
+
+func serveFtpProxy(downstream net.Conn, firstLine string) error {
+	defer func() {
+		downstream.Close()
+		Stats.CurrentTaskNum--
+	}()
+	arr := strings.Split(firstLine, " ")
+	method := arr[0]
+	location := arr[1]
+	Url, err := url.Parse(location)
+	if err != nil {
+		return err
+	}
+	arrHostPort := strings.Split(Url.Host, ":")
+	host := arrHostPort[0]
+	port := 21
+	if len(arrHostPort) > 1 {
+		port, _ = strconv.Atoi(arrHostPort[1])
+	}
+	ftp := new(ftp.FTP)
+
+	//acl
+	ftp.Connect(host, port)
+	ftp.Login(Url.User.Username(), Url.User.Password())
+	if ftp.Code != 230 {
+		return fmt.Errorf(ftp.Message)
+	}
+	ftp.Request("TYPE I")
+	if method == "POST" {
+		// 文件的上传和下载没有实现
+	}
+	return nil
+}
+
+func eatHeader(stream net.Conn) error {
+	for {
+		line, err := readLine(stream)
+		if err != nil {
+			return err
+		}
+		if line == "\r\n" || line == "\n" {
+			break
+		}
+	}
+	return nil
+}
